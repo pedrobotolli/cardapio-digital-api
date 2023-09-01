f908e30cc0cbdbbb0853c724aa23996a7365798b
Pedro Botolli Santos <Pedro.Santos-WDG@ibm.com>
2023-09-01 17:43:31 -0300

    Fixed order Controller, now it creates all the orderItems

--- prisma/migrations/20230901203131_init/migration.sql
+++ prisma/migrations/20230901203131_init/migration.sql
@@ -11,23 +11,26 @@ CREATE TABLE "Product" (
     "name" TEXT NOT NULL,
     "description" TEXT,
     "productCategoryId" INTEGER NOT NULL,
+    "price" DECIMAL NOT NULL,
     CONSTRAINT "Product_productCategoryId_fkey" FOREIGN KEY ("productCategoryId") REFERENCES "ProductCategory" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "OrderItem" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "productId" INTEGER NOT NULL,
     "quantity" INTEGER NOT NULL,
     "additionalInfo" TEXT,
-    "orderId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "orderId" TEXT NOT NULL,
+    CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "Order" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "address" TEXT NOT NULL,
     "telephoneNumber" TEXT NOT NULL,
-    "ordererName" TEXT NOT NULL
+    "ordererName" TEXT NOT NULL,
+    "deliveryTime" DATETIME NOT NULL
 );
--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -1,10 +1,30 @@
 import { db } from '../connectors/prisma';
 import { Request, Response, NextFunction } from 'express';
+import { add } from 'date-fns';
+
 
 export async function createOrder(req: Request, res: Response, next: NextFunction) {
     try {
-        const order = await db.order.create({ data: req.body });
-        return res.status(201).json(order);
+        if (!req.body.items)
+            throw new Error(`Invalid order, the "items" property is required and it must be an array`);
+        console.log(typeof(req.body.items))
+
+        let order = await db.order.create({
+            data: {
+                address: req.body.address,
+                telephoneNumber: req.body.telephoneNumber,
+                ordererName: req.body.ordererName,
+                deliveryTime: add(new Date(), {
+                    minutes: 30
+                }).toJSON()
+            }
+        })
+
+        const orderItems = await db.$transaction(
+            req.body.items.map((item: any) => db.orderItem.create({ data: { ...item, orderId: order.id }, include: {product: true}}))
+        )
+
+        return res.status(201).json({...order, orderItems: orderItems});
     }
     catch (err) {
         next(err)
@@ -23,9 +43,8 @@ export async function getOrders(req: Request, res: Response, next: NextFunction)
 
 export async function getOrderById(req: Request, res: Response, next: NextFunction) {
     try {
-        const order = await db.order.findUnique({ where: { id: Number(req.params.id) } });
-        return order?res.status(200).json(order):res.status(404).json({error: `Cannot find order with id =  ${req.params.id}`})
-    
+        const order = await db.order.findUnique({ where: { id: String(req.params.id) }, include: { orderItems: {include: {product: true}} } });
+        return order ? res.status(200).json(order) : res.status(404).json({ error: `Cannot find order with id =  ${req.params.id}` })
     }
     catch (err) {
         next(err)
@@ -35,14 +54,14 @@ export async function getOrderById(req: Request, res: Response, next: NextFuncti
 
 export async function updateOrder(req: Request, res: Response, next: NextFunction) {
     try {
-        const order = await db.order.findUnique({ where: { id: Number(req.params.id) } });
-        if(!order)
-            return res.status(404).json({error: `Cannot find order with id =  ${req.params.id}`});
-        const updatedOrder = await db.order.update({ where: { id: Number(req.params.id) }, data: req.body });
-        
-        if(updatedOrder){
+        const order = await db.order.findUnique({ where: { id: String(req.params.id) } });
+        if (!order)
+            return res.status(404).json({ error: `Cannot find order with id =  ${req.params.id}` });
+        const updatedOrder = await db.order.update({ where: { id: String(req.params.id) }, data: req.body });
+
+        if (updatedOrder) {
             return res.status(200).json(updatedOrder);
-        }else{
+        } else {
             throw new Error(`Cannot update order`)
         }
     }
@@ -53,9 +72,9 @@ export async function updateOrder(req: Request, res: Response, next: NextFunctio
 
 
 export async function deleteOrder(req: Request, res: Response, next: NextFunction) {
-    try {        
-        const deleteOrder = await db.order.delete({ where: { id: Number(req.params.id) } });
-        return deleteOrder?res.status(200).json(deleteOrder):res.status(404).json({error: `Cannot find order with id =  ${req.params.id}`})
+    try {
+        const deleteOrder = await db.order.delete({ where: { id: String(req.params.id) } });
+        return deleteOrder ? res.status(200).json(deleteOrder) : res.status(404).json({ error: `Cannot find order with id =  ${req.params.id}` })
     }
     catch (err) {
         next(err)
